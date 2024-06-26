21dd9463f8f7d952c69d9d0f990e8dde4e7b482a
Lâm Triệu Vĩ <106202488+vi-lt@users.noreply.github.com>
2024-06-27 04:21:07 +0700

    Update xpath and create step addToCart

--- PlaywrightTests/Page/HomePageObjectModel.cs
+++ PlaywrightTests/Page/HomePageObjectModel.cs
@@ -1,14 +1,25 @@
+using AspectInjector.Broker;
 using Microsoft.Playwright;
 using NUnit.Allure.Attributes;
+using System.Reflection.Metadata.Ecma335;
 
 namespace BigEcommerceApp.Tests.Models;
 
 public class HomePageObjectModel(IPage Page)
 {
     private readonly IPage _page = Page;
-    private ILocator btn_Burger => _page.Locator("//div[@class='bm-burger-button']");
-    private ILocator msg_ErrorLock => _page.Locator("//h3[@data-test='error']");
-    private ILocator menu_left(string nameMenu) => _page.Locator("//a[contains(text(),'" + nameMenu + "')]");
+    private string btn_BurgerXPath = "//div[@class='bm-burger-button']";
+    private string menu_LeftXPath(string nameMenu) => "//a[contains(text(),'" + nameMenu + "')]";
+    private string lbl_NameCartXPath(string nameCart) => "//div[contains(text(),'" + nameCart + "')]";
+    private string btn_AddToCartXPath = "//button[contains(@id,'add')]";
+    private string numb_CartXpath = "//span[@class='shopping_cart_badge']";
+
+    private ILocator btn_Burger => _page.Locator(btn_BurgerXPath);
+    private ILocator menu_Left(string nameMenu) => _page.Locator(menu_LeftXPath(nameMenu));
+    private ILocator lbl_NameCart(string nameCart) => _page.Locator(lbl_NameCartXPath(nameCart));
+    private ILocator btn_AddToCart => _page.Locator(btn_AddToCartXPath);
+    private ILocator numb_Cart => _page.Locator(numb_CartXpath);
+
 
     # region Action
     [AllureStep]
@@ -19,15 +30,35 @@ public class HomePageObjectModel(IPage Page)
 
     public async Task clickOnMenuLeft(string menuName)
     {
-        await menu_left(menuName).ClickAsync();
+        await menu_Left(menuName).ClickAsync();
+    }
+
+    [AllureStep]
+    public async Task clickOnNameCart(string nameCart)
+    {
+        await lbl_NameCart(nameCart).ClickAsync();
+    }
+
+    [AllureStep]
+    public async Task clickButtonAddToCart()
+    {
+        await btn_AddToCart.ClickAsync();
+    }
+
+    [AllureStep]
+    public async Task clickButtonAddToCartBaseOnName(string nameCart)
+    {
+        var xpath = lbl_NameCartXPath(nameCart) + "/ancestor::div[@class='inventory_item_description']" + btn_AddToCartXPath;
+        await _page.Locator(xpath).ClickAsync();
     }
     #endregion
 
     #region Verify
     [AllureStep]
-    public async Task VerifyMessageLockIsVisible()
+    public async Task VerifyNumberAfterClickAddToCart(string expect)
     {
-        await msg_ErrorLock.IsVisibleAsync();
+        string actual = await numb_Cart.TextContentAsync();
+        Assert.AreEqual(actual, expect);
     }
     #endregion
 }
--- PlaywrightTests/Utilities/Constant.cs
+++ PlaywrightTests/Utilities/Constant.cs
@@ -3,4 +3,6 @@ public static class Constant
     public static string userName = "standard_user";
     public static string userNameLock = "locked_out_user";
     public static string password = "secret_sauce";
+
+    public static readonly List<string> cardList = ["Backpack", "Bike Light", "Bolt T-Shirt", "Jacket", "Onesie", "T-Shirt (Red)"];
 }
